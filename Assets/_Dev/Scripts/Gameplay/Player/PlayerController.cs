0af96c48fa1a41621d5692ffe465d655ed118a3b
TungAnh <tung2052003@gmail.com>
2025-02-26 22:56:09 +0700

    add run speed

--- Assets/_Dev/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_Dev/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,35 +1,41 @@
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInput), typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerInput), typeof(Rigidbody), typeof(PlayerAnimation))]
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput playerInput;
     private Rigidbody rb;
+    private PlayerAnimation playerAnimation;
 
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 8f;
     [SerializeField] private float rotationSpeed = 700f;
+
     private void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
-        rb = GetComponent<Rigidbody>();
+        this.playerInput = GetComponent<PlayerInput>();
+        this.rb = GetComponent<Rigidbody>();
+        this.playerAnimation = GetComponent<PlayerAnimation>();
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        this.MovePlayer();
     }
 
     private void MovePlayer()
     {
-        Vector3 inputDirection = playerInput.InputVector;
+        Vector3 inputDirection = this.playerInput.InputVector;
 
         if (inputDirection.magnitude > 0f)
         {
+            float moveSpeed = this.playerAnimation.CurrentSpeed >= 0.5f ? this.runSpeed : this.walkSpeed;
+
             Vector3 movement = inputDirection * moveSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position + movement);
+            this.rb.MovePosition(this.rb.position + movement);
 
             Quaternion targetRotation = Quaternion.LookRotation(inputDirection);
-            rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            this.rb.rotation = Quaternion.RotateTowards(this.rb.rotation, targetRotation, this.rotationSpeed * Time.fixedDeltaTime);
         }
     }
 }
